df9156e28b447424a7e5365499c2d4db480edefa
phpdie <sundaysunsun@vip.qq.com>
2024-01-04 18:02:15 +0800

    update

--- README.md
+++ README.md
@@ -32,7 +32,24 @@ $text = "![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846
  Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划";
 $singleTitle = "阅读全文";
 $singleURL = "https://www.dingtalk.com/";
-$result = $client->sendActionCard($title,$text,$singleTitle,$singleURL);
+$result = $client->sendActionCardSingle($title,$text,$singleTitle,$singleURL);
+print_r($result);
+*/
+
+/*
+$btn1['title'] = '百度1';
+$btn1['actionURL'] = 'https://www.baidu.com';
+
+$btn2['title'] = '百度2';
+$btn2['actionURL'] = 'https://www.baidu.com';
+
+$title = '乔布斯 20 年前想打造一间苹果咖啡厅，而它正是 Apple Store 的前身';
+$text = "![screenshot](https://gw.alicdn.com/tfs/TB1ut3xxbsrBKNjSZFpXXcXhFXa-846-786.png)
+ ### 乔布斯 20 年前想打造的苹果咖啡厅 
+ Apple Store 的设计正从原来满满的科技感走向生活化，而其生活化的走向其实可以追溯到 20 年前苹果一个建立咖啡馆的计划";
+$btnOrientation = 0;//0：按钮竖直排列  1：按钮横向排列
+$btns = [(object)$btn1, (object)$btn2];
+$result = $client->sendActionCardMulti($title, $text, $btnOrientation, $btns);
 print_r($result);
 */
 
@@ -45,7 +62,7 @@ $link2['title'] = '百度2';
 $link2['messageURL'] = 'https://www.baidu.com';
 $link2['picURL'] = 'https://www.baidu.com/img/flexible/logo/pc/result.png';
 
-$links = [(object)$link1,(object)$link2];
-$result = $client->sendFeedCard($feedCard);
+$links = [(object)$link1, (object)$link2];
+$result = $client->sendFeedCard($links);
 print_r($result);
 */
--- src/DingDing.php
+++ src/DingDing.php
@@ -4,105 +4,72 @@ namespace Notify;
 
 class DingDing
 {
-    private $msgtype;
+    private $msgtype = 'empty';
 
-    private $webhook;
+    private $webhook = '';
 
-    private $ssl;
+    private $ssl = true;
 
     private $isAtAll = false;
+
     private $atMobiles = [];
+
     private $atUserIds = [];
 
-    public function __construct($webhook = '', $ssl = true, $msgtype = '')
+    public function __construct($webhook = '', $ssl = true, $msgtype = 'empty')
     {
-        if ($webhook) {
-            $this->setWebhook($webhook);
-        }
-        if ($msgtype) {
-            $this->setMsgtype($msgtype);
-        }
+        $this->setWebhook($webhook);
+        $this->setMsgtype($msgtype);
         $this->setSsl($ssl);
     }
 
-    public function isAtAll()
-    {
-        return $this->isAtAll;
-    }
-
-    public function setIsAtAll($isAtAll)
+    public function setIsAtAll($isAtAll = false)
     {
         $this->isAtAll = $isAtAll;
         return $this;
     }
 
-    public function getAtMobiles()
-    {
-        return $this->atMobiles;
-    }
-
-    public function setAtMobiles($atMobiles)
+    public function setAtMobiles($atMobiles = [])
     {
         $this->atMobiles = $atMobiles;
         return $this;
     }
 
-    public function getAtUserIds()
-    {
-        return $this->atUserIds;
-    }
-
-    public function setAtUserIds(array $atUserIds)
+    public function setAtUserIds($atUserIds = [])
     {
         $this->atUserIds = $atUserIds;
         return $this;
     }
 
-    public function getMsgtype()
-    {
-        return $this->msgtype;
-    }
-
-    /** https://open.dingtalk.com/document/isvapp/message-type
-     * @param $msgtype
-     * @return void
-     */
-    public function setMsgtype($msgtype)
+    public function setMsgtype($msgtype = 'empty')
     {
         $this->msgtype = $msgtype;
+        return $this;
     }
 
-    public function getWebhook()
-    {
-        return $this->webhook;
-    }
-
-    public function setWebhook($webhook)
+    public function setWebhook($webhook = '')
     {
         $this->webhook = $webhook;
+        return $this;
     }
 
-    public function isSsl()
-    {
-        return $this->ssl;
-    }
-
-    public function setSsl($ssl)
+    public function setSsl($ssl = false)
     {
         $this->ssl = $ssl;
+        return $this;
     }
 
     public function send($post_string)
     {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->getWebhook());
+        curl_setopt($ch, CURLOPT_URL, $this->webhook);
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json;charset=utf-8'));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json;charset=utf-8']);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         // 线下环境不用开启curl证书验证, 未调通情况可尝试添加该代码
-        if ($this->isSsl() === false) {
+        if ($this->ssl === false) {
             curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         }
@@ -119,25 +86,44 @@ class DingDing
                 $data[$key] = $val;
             }
         }
-        $data['msgtype'] = $this->getMsgtype();
+        $data['msgtype'] = $this->msgtype;
         return json_encode($data);
     }
 
     private function formatAt()
     {
-        $at['isAtAll'] = $this->isAtAll();
-        $at['atMobiles'] = $this->getAtMobiles();
-        $at['atUserIds'] = $this->getAtUserIds();
+        $at = [];
+        foreach (['isAtAll', 'atMobiles', 'atUserIds'] as $val) {
+            if ($this->{$val}) {
+                $at[$val] = $this->{$val};
+            }
+        }
         return $at;
     }
 
+    /** 文本text类型
+     * @param $text
+     * @return bool|string
+     */
     public function sendText($text)
     {
         $this->setMsgtype('text');
-        $post_string = $this->formatData(['text' => ['content' => $text], 'at' => $this->formatAt()]);
+        $post_string = $this->formatData([
+            'text' => [
+                'content' => $text
+            ],
+            'at' => $this->formatAt()
+        ]);
         return $this->send($post_string);
     }
 
+    /** 链接Link类型
+     * @param $title
+     * @param $text
+     * @param $messageUrl
+     * @param $picUrl
+     * @return bool|string
+     */
     public function sendLink($title, $text, $messageUrl, $picUrl = '')
     {
         $this->setMsgtype('link');
@@ -152,14 +138,32 @@ class DingDing
         return $this->send($post_string);
     }
 
+    /** Markdown类型
+     * @param $title
+     * @param $text
+     * @return bool|string
+     */
     public function sendMarkdown($title, $text)
     {
         $this->setMsgtype('markdown');
-        $post_string = $this->formatData(['markdown' => ['title' => $title, 'text' => $text], 'at' => $this->formatAt()]);
+        $post_string = $this->formatData([
+            'markdown' => [
+                'title' => $title,
+                'text' => $text
+            ],
+            'at' => $this->formatAt()
+        ]);
         return $this->send($post_string);
     }
 
-    public function sendActionCard($title, $text, $singleTitle, $singleURL)
+    /** 整体跳转ActionCard类型
+     * @param $title
+     * @param $text
+     * @param $singleTitle
+     * @param $singleURL
+     * @return bool|string
+     */
+    public function sendActionCardSingle($title, $text, $singleTitle, $singleURL)
     {
         $this->setMsgtype('actionCard');
         $post_string = $this->formatData([
@@ -173,10 +177,39 @@ class DingDing
         return $this->send($post_string);
     }
 
+    /** 独立跳转ActionCard类型
+     * @param $title
+     * @param $text
+     * @param $btnOrientation 按钮排列顺序0：按钮竖直排列1：按钮横向排列
+     * @param $btns [{"title": "内容不错","actionURL": "https://www.dingtalk.com/"},...]
+     * @return bool|string
+     */
+    public function sendActionCardMulti($title, $text, $btnOrientation, $btns)
+    {
+        $this->setMsgtype('actionCard');
+        $post_string = $this->formatData([
+            'actionCard' => [
+                'title' => $title,
+                'text' => $text,
+                'btnOrientation' => $btnOrientation,
+                'btns' => $btns
+            ]
+        ]);
+        return $this->send($post_string);
+    }
+
+    /** FeedCard类型
+     * @param $feedCard [{"title": "时代的火车向前开1","messageURL": "https://www.dingtalk.com/","picURL": "https://img.alicdn.com/a.png"},...]
+     * @return bool|string
+     */
     public function sendFeedCard($feedCard)
     {
         $this->setMsgtype('feedCard');
-        $post_string = $this->formatData(['feedCard' => ['links' => $feedCard]]);
+        $post_string = $this->formatData([
+            'feedCard' => [
+                'links' => $feedCard
+            ]
+        ]);
         return $this->send($post_string);
     }
 
